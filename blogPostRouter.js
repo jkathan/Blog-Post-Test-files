4a5590dfbfdf701002102d3d56532c37c6525b69
jkathan <34135462+jkathan@users.noreply.github.com>
2018-07-26 17:19:25 -0400

    Update blogPostRouter.js

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -7,8 +7,13 @@ const jsonParser = bodyParser.json();
 
 const {BlogPost} = require('./models');
 
-BlogPost.create('this is the first blog post');
-BlogPost.create('this is the second blog post');
+function lorem() {
+  return ('text text text');
+  )
+  
+
+BlogPost.create('this is the first blog post', lorem(), "Joe Schmoo");
+BlogPost.create('this is the second blog post', lorem(), "Tera Cota");
 
 router.get('/', (req, res) => {
   res.json(BlogPost.get());
@@ -16,7 +21,7 @@ router.get('/', (req, res) => {
 
 router.post('/', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
-  const requiredFields = ["id", 'entry'];
+  const requiredFields = ["title", "content", "author"];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -25,7 +30,11 @@ router.post('/', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const item = BlogPost.create(req.body.post);
+  const item = BlogPost.create(
+    req.body.title,
+    req.body.content,
+    req.body.author
+  );
   res.status(201).json(item);
 });
 
@@ -36,7 +45,7 @@ router.delete('/:id', (req, res) => {
 });
 
 router.put('/:id', jsonParser, (req, res) => {
-  const requiredFields = ["id", 'entry'];
+  const requiredFields = ["id", "title", "content", "author", "publishDate"];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -55,7 +64,10 @@ router.put('/:id', jsonParser, (req, res) => {
   console.log(`Updating shopping list item \`${req.params.id}\``);
   const updatedItem = BlogPost.update({
     id: req.params.id,
-    post: req.body.entry,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
   });
   res.status(204).end();
 })
