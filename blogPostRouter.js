a4f70025a437aa964a920b77798d69b16cb36871
Joey Kathan <kathan.joey@gmail.com>
2018-07-26 16:54:31 -0400

    initial commit

---
+++ blogPostRouter.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+
+const {BlogPost} = require('./models');
+
+BlogPost.create('this is the first blog post');
+BlogPost.create('this is the second blog post');
+
+router.get('/', (req, res) => {
+  res.json(BlogPost.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['entry'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.post);
+  res.status(201).json(item);
+});
+
+router.delete('/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted blog post item \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['entry'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  const updatedItem = Recipes.update({
+    id: req.params.id,
+    post: req.body.post,
+  });
+  res.status(204).end();
+})
+
---
+++ server.js
@@ -0,0 +1,17 @@
+const app = express();
+
+const blogPostRouter = require('./blogPostRouter');
+
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use('/blog-posts', blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
